d66287184aabf3ce4d4cc83c6d92bbb49214017f
Blake La Pierre <blakelapierre@gmail.com>
2016-03-30 16:29:32 -0700

    Fixes

--- bin/bnw.sh
+++ bin/bnw.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+project_name=$(basename $(pwd))
 
-cd node_modules/base-node-workspace && ./add_project ../.. $(basename ../..) && ./dev_project $(basename ../..)
+cd node_modules/base-node-workspace && ./remove_project $project_name && ./add_project $project_name && ./dev_project $project_name
