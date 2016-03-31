7fb0979b6279565ebdb5a3702e5c2cbe46c0cda1
Blake La Pierre <blakelapierre@gmail.com>
2016-03-30 17:07:37 -0700

    Need to go up a dir

--- bin/bnw.sh
+++ bin/bnw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 project_name=$(basename $(pwd))
 
-cd node_modules/base-node-workspace && ./remove_project $project_name && ./add_project $project_name && ./dev_project $project_name
+cd node_modules/base-node-workspace && ./remove_project $project_name && ./add_project ../$project_name && ./dev_project $project_name
